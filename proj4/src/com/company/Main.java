d7c38021d0ba86ef9b51b414290eb59eadfee19a
fullstack290623 <fullstack290623@gmail.com>
2024-06-02 18:21:20 +0300

    .

--- proj4/src/com/company/Main.java
+++ proj4/src/com/company/Main.java
@@ -2,7 +2,16 @@ package com.company;
 
 public class Main {
 
+    // overloading
+    private static int add(int x, int y) {
+        return x + y;
+    }
+    private static float add(float x, float y) {
+        return x + y;
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(add(1, 3.3f));
+        System.out.println(add(1.3f, 9.88f));
     }
 }
